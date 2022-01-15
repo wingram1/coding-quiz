c97d8b184de4b2f8d868364692713d7fdec2f4b2
wingram1 <waingram96@gmail.com>
2022-01-15 16:35:51 -0600

    question prompt now recognizes the id of a clicked answer

--- assets/script.js
+++ assets/script.js
@@ -91,8 +91,6 @@ var nextQuestion = function(questionIndex) {
     questionEl.innerHTML = currentQuestion.question;
     container.appendChild(questionEl);
 
-    debugger;
-
     var answerChoices = [
         currentQuestion.answerCorrect,
         currentQuestion.wrong1,
@@ -103,30 +101,57 @@ var nextQuestion = function(questionIndex) {
     //shuffle answers
     shuffleAnswers(answerChoices)
 
+    console.log("currentQuestion.answerCorrect: " + currentQuestion.answerCorrect);
+
+    var chooseAnswerHandler = function(pickedAnswer) {
+        //log pickedAnswer to console
+        console.log(pickedAnswer.innerHTML + " has been chosen.");
+
+        //see if answer is correct or wrong
+        if (pickedAnswer.innerHTML == parseInt(currentQuestion.answerCorrect)) {
+            console.log("You picked the correct answer.");
+        }
+        else if (pickedAnswer.innerHTML == currentQuestion.wrong1 || pickedAnswer.innerHTML == currentQuestion.wrong2 || pickedAnswer.innerHTML == currentQuestion.wrong3) {
+            console.log("You picked the wrong answer.");
+        }
+        else { //checking for errors
+            console.log("pickedAnswer: " + pickedAnswer.innerHTML);
+        }//todo: seperate the innerHTML of A: and answerChoices[] when creating the answer elements
+    };
+
     //create answer elements
     var answerA = document.createElement("h2");
-    answerA.className = "content";
-    answerA.id= "answer";
+    answerA.className = "content answer";
+    answerA.id= "answerA";
     answerA.innerHTML = "A: " + answerChoices[0];
     container.appendChild(answerA);
+    answerA.addEventListener("click", function(){chooseAnswerHandler(answerA); });
 
     var answerB = document.createElement("h2");
-    answerB.className = "content";
-    answerB.id= "answer";
+    answerB.className = "content answer";
+    answerB.id= "answerB";
     answerB.innerHTML = "B: " + answerChoices[1];
     container.appendChild(answerB);
+    answerB.addEventListener("click", function(){chooseAnswerHandler(answerB); });
 
     var answerC = document.createElement("h2");
-    answerC.className = "content";
-    answerC.id= "answer";
+    answerC.className = "content answer";
+    answerC.id= "answerC";
     answerC.innerHTML = "C: " + answerChoices[2];
     container.appendChild(answerC);
+    answerC.addEventListener("click", function(){chooseAnswerHandler(answerC); });
 
     var answerD = document.createElement("h2");
-    answerD.className = "content";
-    answerD.id= "answer";
+    answerD.className = "content answer";
+    answerD.id= "answerD";
     answerD.innerHTML = "D: " + answerChoices[3];
     container.appendChild(answerD);
+    answerD.addEventListener("click", function(){chooseAnswerHandler(answerD); });
+
+    // var options = document.getElementsByClassName("answer");
+
+    //todo: add event listener outside of function;
+    // answerA.addEventListener("click",chooseAnswerHandler(answerA));
 
     //TODO: let user choose answer
     //TODO: if pickedAnswer === currentQuestion.answerCorrect, then userScore++ && NextQuestion(); && display correct
@@ -178,3 +203,5 @@ var shuffleAnswers = function(answerBank) {
 
 //button press to delete html inside content creator and start generating questions
 startButton.addEventListener("click", startButtonHandler);
+
+//answerA.addEventListener("click",chooseAnswerHandler(answerA));
