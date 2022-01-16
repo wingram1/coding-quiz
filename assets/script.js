7f999c8c246987f74852b8ad8091c501308362ba
wingram1 <waingram96@gmail.com>
2022-01-15 19:57:02 -0600

    quiz will end and total up score when either there are no more questions or the time has run out

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 //set up variables based on html id's
-var timeLeft = document.querySelector("#time-left");
 var highScoresDisplay = document.querySelector("#high-scores");
 var startButton = document.querySelector("#start-button");
 var container = document.querySelector(".content-container");
-var currentQuestion = 0;
+// var currentQuestion = 0;
+var questionMarker = 0;
 var userScore = 0;
+var timeLeft = 75;
 
 //create bank of question objects (array)
 var questionBank = [
@@ -50,7 +51,6 @@ var questionBank = [
 var deleteContent = function() {
 
     var content = document.getElementsByClassName("content");
-        console.log(content);
         while(content.length > 0){
             content[0].parentNode.removeChild(content[0]);
         }
@@ -61,16 +61,18 @@ var deleteContent = function() {
 var startButtonHandler = function(event) {
     var targetEl = event.target;
     console.log("startButton has been clicked.");
-    var timeLeft = 10;
+    // var timeLeft = 75;
     
     //start timer
     var countDown = setInterval(function() {
         document.querySelector("#time-left").innerHTML = "Time Left: " + timeLeft;
         timeLeft--;
     
-        if (timeLeft < 0) {
+        if (timeLeft <= 0) {
+            //stop timer and finish quiz
             clearInterval(countDown);
             console.log("countDown has completed");
+            // finishQuiz();
         }
     }, 1000);
 
@@ -78,88 +80,96 @@ var startButtonHandler = function(event) {
     deleteContent();
 
     //Start nextQuestion function
-    nextQuestion(currentQuestion);
+    nextQuestion(0);
 };
 
 //create question generation function
 var nextQuestion = function(questionIndex) {
-    var currentQuestion = questionBank[questionIndex];
+    if (questionIndex <= (questionBank.length - 1)) {
+        var currentQuestion = questionBank[questionIndex];
 
-    var questionEl = document.createElement("h1");
-    questionEl.className = "content";
-    questionEl.id= "question";
-    questionEl.innerHTML = currentQuestion.question;
-    container.appendChild(questionEl);
+        var questionEl = document.createElement("h1");
+        questionEl.className = "content";
+        questionEl.id= "question";
+        questionEl.innerHTML = currentQuestion.question;
+        container.appendChild(questionEl);
 
-    var answerChoices = [
-        currentQuestion.answerCorrect,
-        currentQuestion.wrong1,
-        currentQuestion.wrong2,
-        currentQuestion.wrong3
-    ];
+        var answerChoices = [
+            currentQuestion.answerCorrect,
+            currentQuestion.wrong1,
+            currentQuestion.wrong2,
+            currentQuestion.wrong3
+        ];
 
-    //shuffle answers
-    shuffleAnswers(answerChoices)
+        //shuffle answers
+        shuffleAnswers(answerChoices)
 
-    console.log("currentQuestion.answerCorrect: " + currentQuestion.answerCorrect);
+        console.log("currentQuestion.answerCorrect: " + currentQuestion.answerCorrect);
 
-    var chooseAnswerHandler = function(pickedAnswer) {
-        //log pickedAnswer to console
-        console.log(pickedAnswer + " has been chosen.");
+        var chooseAnswerHandler = function(pickedAnswer) {
+            //log pickedAnswer to console
+            console.log(pickedAnswer + " has been chosen.");
 
-        //see if answer is correct or wrong
-        if (pickedAnswer == parseInt(currentQuestion.answerCorrect)) {
-            console.log("You picked the correct answer.");
-        }
-        else if (pickedAnswer == currentQuestion.wrong1 || pickedAnswer== currentQuestion.wrong2 || pickedAnswer == currentQuestion.wrong3) {
-            console.log("You picked the wrong answer.");
-        }
-    };
+            //see if answer is correct or wrong
+            if (pickedAnswer == currentQuestion.answerCorrect) {
+                console.log("You picked the correct answer.");
 
-    //create answer elements
-    var answerA = document.createElement("h2");
-    answerA.className = "content answer";
-    answerA.id= "answerA";
-    answerA.innerHTML = "A: " + answerChoices[0];
-    container.appendChild(answerA);
-    answerA.addEventListener("click", function(){chooseAnswerHandler(answerChoices[0]); });
+                //increase user score, delete content and cycle to next question
+                userScore += 5;
 
-    var answerB = document.createElement("h2");
-    answerB.className = "content answer";
-    answerB.id= "answerB";
-    answerB.innerHTML = "B: " + answerChoices[1];
-    container.appendChild(answerB);
+                deleteContent();
+                questionMarker++;
+                nextQuestion(questionMarker);
+            }
+            else if (pickedAnswer == currentQuestion.wrong1 || pickedAnswer== currentQuestion.wrong2 || pickedAnswer == currentQuestion.wrong3) {
+                //delete content and cycle to next question without increasing user score
+                console.log("You picked the wrong answer.");
+                deleteContent();
+                questionMarker++;
+                nextQuestion(questionMarker);
+            }
+        };
+
+        //create answer elements
+        var answerA = document.createElement("h2");
+        answerA.className = "content answer";
+        answerA.id= "answerA";
+        answerA.innerHTML = "A: " + answerChoices[0];
+        container.appendChild(answerA);
+        answerA.addEventListener("click", function(){chooseAnswerHandler(answerChoices[0]); });
+
+        var answerB = document.createElement("h2");
+        answerB.className = "content answer";
+        answerB.id= "answerB";
+        answerB.innerHTML = "B: " + answerChoices[1];
+        container.appendChild(answerB);
     answerB.addEventListener("click", function(){chooseAnswerHandler(answerChoices[1]); });
 
-    var answerC = document.createElement("h2");
-    answerC.className = "content answer";
-    answerC.id= "answerC";
-    answerC.innerHTML = "C: " + answerChoices[2];
-    container.appendChild(answerC);
-    answerC.addEventListener("click", function(){chooseAnswerHandler(answerChoices[2]); });
+        var answerC = document.createElement("h2");
+        answerC.className = "content answer";
+        answerC.id= "answerC";
+        answerC.innerHTML = "C: " + answerChoices[2];
+        container.appendChild(answerC);
+        answerC.addEventListener("click", function(){chooseAnswerHandler(answerChoices[2]); });
 
-    var answerD = document.createElement("h2");
-    answerD.className = "content answer";
-    answerD.id= "answerD";
-    answerD.innerHTML = "D: " + answerChoices[3];
-    container.appendChild(answerD);
-    answerD.addEventListener("click", function(){chooseAnswerHandler(answerChoices[3]); });
-
-    // var options = document.getElementsByClassName("answer");
-
-    //todo: add event listener outside of function;
-    // answerA.addEventListener("click",chooseAnswerHandler(answerA));
-
-    //TODO: let user choose answer
-    //TODO: if pickedAnswer === currentQuestion.answerCorrect, then userScore++ && NextQuestion(); && display correct
-        //todo else if pickedAnswer === currentQuestion.answerWrong1/2/3, then nextQuestion(); && display correct
-
-    //TODO: when no more questions, display score and have user enter name to be stored in localStorage
-    //TODO: display highscores in localStorage
-
-    //TODO: add more questions (not important rn, just get code functioning)
+        var answerD = document.createElement("h2");
+        answerD.className = "content answer";
+        answerD.id= "answerD";
+        answerD.innerHTML = "D: " + answerChoices[3];
+        container.appendChild(answerD);
+        answerD.addEventListener("click", function(){chooseAnswerHandler(answerChoices[3]); });
 
 
+
+        //TODO: when no more questions, display score and have user enter name to be stored in localStorage
+        //TODO: display highscores in localStorage
+
+        //TODO: add more questions (not important rn, just get code functioning)
+    
+    }
+    else {
+        finishQuiz();
+    }
 };
 
 //function to put answer properties into an array and shuffle
@@ -189,10 +199,18 @@ var shuffleAnswers = function(answerBank) {
     return answerBank;
 }; 
 
-//TODO: have nextQuestion() pause the for loop to allow the user to answer the question
+var finishQuiz = function() {
+    console.log("You have finished the quiz! :)");
 
+    //add existing userScore with time Left
+    console.log("Time Left: " + timeLeft);
+    console.log("Score from answers: " + userScore);
+    userScore += timeLeft;
+    //stop timer
+    timeLeft = 0;
+    console.log("Your score was: " + userScore);
+}
 
-//TODO: create finish screen (upload score and name to localStorage)
 
 
 //TODO: create high scores screen (get from localStorage, display on the screen)
